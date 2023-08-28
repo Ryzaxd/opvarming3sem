541808d9d667103bc14333d755f4e46cc5da1960
Ryzaxd <nick986h@edu.zealand.dk>
2023-08-28 11:47:38 +0200

    virker med decimaltal, inputMismatchException og exit når man trykker på "q"

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,57 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class Main {
 
-    // TODO Få det til at virke med decimaltal +
-    // TODO Få det til ikke at crashe med en runtime exception, når man ved en fejl indtaster x
+    // TODO Få det til at virke med decimaltal (tjek)
+    // TODO Få det til ikke at crashe med en runtime exception, når man ved en fejl indtaster x (tjek)
     // TODO Få det til at stoppe når man skriver stop
 
     public static void main(String[] args) {
         System.out.println("Hej med dig!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Indtast et tal: ");
-        double x = scanner.nextDouble();
-        System.out.print("Indtast endnu et tal: ");
-        double y = scanner.nextDouble();
-        double sum = x + y;
-        double antalTal = 2;
-        double resultat = sum / antalTal;
-        System.out.println("Gennemsnittet af alle tal er: " + resultat);
-        for (int i = 0; i < 10; i--) {
+        Scanner scanner = new Scanner(in);
+
+        try {
+            System.out.print("Indtast et tal: ");
+            double x = scanner.nextDouble();
             System.out.print("Indtast endnu et tal: ");
-            y = scanner.nextDouble();
-            sum = sum + y;
-            resultat = sum/ ++antalTal;
+            double y = scanner.nextDouble();
+            double sum = x + y;
+            double antalTal = 2;
+            double resultat = sum / antalTal;
             System.out.println("Gennemsnittet af alle tal er: " + resultat);
+
+            for (int i = 0; i < 10; i--) {
+                try {
+                    System.out.print("Indtast endnu et tal: ");
+                    y = scanner.nextDouble();
+                    sum = sum + y;
+                    resultat = sum / ++antalTal;
+                    System.out.println("Gennemsnittet af alle tal er: " + resultat);
+
+                    String input = scanner.next();
+                    if (input.equalsIgnoreCase("q")) {
+                        System.out.println("Programmet afsluttes.");
+                        scanner.close();
+                        System.exit(0);
+                    }
+
+                } catch (InputMismatchException e) {
+                    System.out.println("Fejl: Indtast venligst et tal.");
+                    scanner.next();
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Fejl: Indtast venligst et tal.");
         }
 
+        String input = scanner.next();
+        if (input.equalsIgnoreCase("q")) {
+            System.out.println("Programmet afsluttes.");
+            scanner.close();
+            System.exit(0);
+        }
     }
 }
